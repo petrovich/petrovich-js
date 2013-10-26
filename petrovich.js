481adb13be646bc11403706cc80f417b70f9fa5d
Ilia Schelokov <thaumant@gmail.com>
2013-10-26 17:38:13 +0400

    Translated all functionality, without checking for correctness yet

--- petrovich.js
+++ petrovich.js
@@ -2,20 +2,22 @@
 
 (function(){
 
-    // Predefined proper values
+    // Predefined values
     var predef = {
         genders: ['male', 'female', 'androgynous'],
         nametypes: ['first', 'last', 'middle'],
         cases: ['nominative', 'genitive', 'dative', 'accusative', 'instrumental', 'prepositional']
     };
 
+    // Auxiliary function, used by validate() and find_rule_local()
+    function contains(x, arr) {
+        for (var i in arr) { if (arr[i] === x) return true; }
+        return false;
+    }
+
     // Validates that gender and case are members of predef
     // No Array.indexOf owing to IE8
     function validate(gender, gcase) {
-        function contains(x, arr) {
-            for (var i in arr) { if (arr[i] === x) return true; }
-            return false;
-        }
         if (!contains(predef.genders, gender))
             throw new Error('Invalid gender: ' + gender);
         if (!contains(predef.cases, gcase))
@@ -38,7 +40,8 @@
             if (!petrovich[gender]) petrovich[gender] = {};
             for (var k in predef.nametypes) {
                 var nametype = predef.nametypes[k];
-                if (!petrovich[gender][nametype]) petrovich[gender][nametype] = {};
+                if (!petrovich[gender][nametype])
+                    petrovich[gender][nametype] = {};
                 for (var l in predef.cases) {
                     var gcase = predef.cases[l];
                     // The flower on the mountain peak:
@@ -58,10 +61,95 @@
     else if (window) window.kuzmich = kuzmich;
 
 
-    /*function inflect (gender, name, gcase, nametype) {
-        var nametype_rulesets = rules[nametype];
-        var i = 0;
+
+
+    // Key private method, used by all public methods
+    function inflect (gender, name, gcase, nametype) {
+        var nametype_rulesets = rules[nametype],
+            parts = name.split('-'),
+            result = [];
+            for (var k in parts) {
+                var part = parts[k],
+                    first_word = k === 0 && parts.size > 1,
+                    rule = find_rule_global(gender, name,
+                        nametype_rulesets, {first_word: first_word});
+                if (rule) result.push(apply_rule(name, gcase, rule));
+                else result.push(name);
+            }
+            return result.join('-');
+    }
+
+
+    // Find groups of rules in exceptions or suffixes of given nametype
+    function find_rule_global(gender, name, nametype_rulesets, features) {
+        if (!features) features = {};
+        var tags = [];
+        for (var key in features) {
+            if (features[key]) tags.push(key);
+        }
+        if (nametype_rulesets.exceptions) {
+            var rule = find_rule_local(
+                gender, name, nametype_rulesets.exceptions, true, tags);
+            if (rule) return rule;
+        }
+        return find_rule_local(
+            gender, name, nametype_rulesets.suffixes, false, tags);
+    };
+
+
+    // Local search in rulesets of exceptions or suffixes
+    function find_rule_local(gender, name, ruleset, match_whole_word, tags) {
+        for (var i in ruleset) {
+            var rule = ruleset[i];
+
+            if (rule.tags) {
+                var common_tags = [];
+                for (var k in rule.tags) {
+                    var tag = rule.tags[k];
+                    if (!contains(tags, tag)) common_tags.push(tag);
+                }
+                if (!common_tags.length) continue;
+            }
+            if (rule.gender !== 'androgynous' && gender !== rule.gender)
+                continue;
+
+            name = name.toLowerCase();
+            for (var k in rule.test) {
+                var sample = rule.test[k];
+                var test = match_whole_word ? name :
+                    name.substr(name.length - sample.length);
+                if (test === sample) return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Apply found rule to given name
+    // Move error throwing from this function to API method
+    function apply_rule(name, gcase, rule) {
+        var mod;
+        if (gcase === 'nominative') mod = '.';
+        else if (contains(predef.cases, gcase)) {
+            for (var i in predef.cases) {
+                if (gcase === predef.cases[i]) {
+                    mod = rule.mods[i-1];
+                    break;
+                }
+            }
+        } else throw new Error('Unknown grammatic case: ' + gcase);
         
-    }*/
+        for (var i in mod) {
+            var chr = ;
+            switch (chr) {
+                case '.': break;
+                case '-':
+                    name = name.substr(0, name.length-1);
+                    break;
+                default: name += chr;
+            }
+        }
+        return name;
+    }
 
 })();
